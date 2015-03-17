6668e82a3194c7879629d2a531fde51ebeb1e87c
Jan Sedivy <jansedivy@abdoc.net>
2015-03-17 21:43:42 +0100

    change styling of record item

--- app/components/record-item.jsx
+++ app/components/record-item.jsx
@@ -4,9 +4,14 @@ var Link = require('react-router').Link;
 var RecordItem = React.createClass({
   render() {
     return (
-      <div>
+      <div className="record-item">
         <span className="score">{this.props.data.score}</span>
-        <a href={this.props.data.url}>{this.props.data.title}</a> <Link to="comments" params={{ name: this.props.data.subreddit, id: this.props.data.id }}>{this.props.data.commentCounts} Comments</Link>
+        <div className="info">
+          <a href={this.props.data.url}>{this.props.data.title}</a>
+          <div className="meta">
+            <Link to="comments" params={{ name: this.props.data.subreddit, id: this.props.data.id }}>{this.props.data.commentCounts} Comments</Link>
+          </div>
+        </div>
       </div>
     );
   }
