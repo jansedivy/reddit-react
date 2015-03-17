717c6e750c1d483b7ca548047d5ec2c7d46a208c
Jan Sedivy <jansedivy@abdoc.net>
2015-03-17 19:55:45 +0100

    show what are you searching for

--- app/components/search-view.jsx
+++ app/components/search-view.jsx
@@ -5,7 +5,8 @@ var Reddit = require('../reddit');
 var SearchView = React.createClass({
   getInitialState() {
     return {
-      searchItems: []
+      searchItems: [],
+      searchQuery: ''
     };
   },
 
@@ -16,13 +17,17 @@ var SearchView = React.createClass({
 
     Reddit.search(input).then((data) => this.setState({ searchItems: data }));
 
+    this.setState({
+      searchQuery: input
+    });
+
     this.refs.search.getDOMNode().value = '';
   },
 
   render() {
     return (
       <div>
-        <h1>Hello</h1>
+        <h1>Searching for: {this.state.searchQuery}</h1>
         <form className="search-form" onSubmit={this.handleSubmit}>
           <input type="text" placeholder="search" ref="search"/>
           <input type="submit" value="Search"/>
