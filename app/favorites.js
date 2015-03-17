06ad3f937ee8ba33f5b02812160e7d1518b34540
Jan Sedivy <jansedivy@abdoc.net>
2015-03-17 21:39:29 +0100

    don't allow duplicities in favorites

--- app/favorites.js
+++ app/favorites.js
@@ -6,6 +6,14 @@ var Favorites = {
   },
 
   add(name) {
+    var existingItems = Favorites.all().filter(item => {
+      return item.name.toLowerCase() === name.toLowerCase();
+    });
+
+    if (existingItems.length) {
+      return existingItems[0];
+    }
+
     return store.set('favorites', Favorites.all().concat({ name }));
   }
 };
