943a442518fd998b78e66c6027575af21fc3fa54
Jan Sedivy <jansedivy@abdoc.net>
2015-03-18 10:17:49 +0100

    add 404 page

---
+++ app/components/not-found.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var NotFound = React.createClass({
+  render() {
+    return <h1>404 boys</h1>;
+  }
+});
+
+module.exports = NotFound;
--- app/routes.jsx
+++ app/routes.jsx
@@ -3,19 +3,25 @@ var Router = require('react-router');
 
 var Route = Router.Route;
 var DefaultRoute = Router.DefaultRoute;
+var NotFoundRoute = Router.NotFoundRoute;
 
 var App = require('./components/app');
 var SubredditView = require('./components/subreddit-view');
 var FavoritesView = require('./components/favorites-view');
 var CommentsView = require('./components/comments-view');
 var SearchView = require('./components/search-view');
+var NotFound = require('./components/not-found');
 
 var routes = (
-  <Route name="app" path="/" handler={App}>
-    <DefaultRoute handler={FavoritesView}/>
-    <Route name="search" path="search" handler={SearchView}/>
-    <Route name="subreddit" path="r/:name" handler={SubredditView}/>
-    <Route name="comments" path="r/:name/comments/:id" handler={CommentsView}/>
+  <Route>
+    <Route name="app" path="/" handler={App}>
+      <DefaultRoute handler={FavoritesView}/>
+      <Route name="search" path="search" handler={SearchView}/>
+      <Route name="subreddit" path="r/:name" handler={SubredditView}/>
+      <Route name="comments" path="r/:name/comments/:id" handler={CommentsView}/>
+    </Route>
+
+    <NotFoundRoute handler={NotFound}/>
   </Route>
 );
 
