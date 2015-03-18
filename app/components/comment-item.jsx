de22694067d7cff64924428673d25cec9f1fa0c3
Jan Sedivy <jansedivy@abdoc.net>
2015-03-18 16:58:22 +0100

    add button for toggling nested comments

--- app/components/comment-item.jsx
+++ app/components/comment-item.jsx
@@ -3,18 +3,41 @@ var Markdown = require('./markdown');
 var DateFormat = require('./date-format');
 
 var CommentItem = React.createClass({
-  render() {
+  getInitialState() {
+    return {
+      showNested: true
+    };
+  },
+
+  toggleNestedComments(e) {
+    e.preventDefault();
+
+    this.setState({
+      showNested: !this.state.showNested
+    });
+  },
+
+  getNestedComments() {
     var nestedComments = this.props.data.comments.map(function(item) {
       return <CommentItem data={item} key={item.id}/>;
     });
 
+    if (!this.state.showNested || !nestedComments.length) {
+      return;
+    }
+
+    return <div className="nested-comments">{nestedComments}</div>;
+  },
+
+  render() {
     return (
       <div className="comment">
         <div className="comment-text">
           <h4>{this.props.data.score} - {this.props.data.author} <span className="comment-date"><DateFormat date={this.props.data.created}/></span></h4>
           <Markdown data={this.props.data.text}/>
+          <a href="#" className="toggle-comment-visiblity" onClick={this.toggleNestedComments}></a>
         </div>
-        <div className="nested-comments">{nestedComments}</div>
+        {this.getNestedComments()}
       </div>
     );
   }
