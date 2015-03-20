2916b0f5e81f489ce27a573388e7bb8ed3e49ac2
Jan Sedivy <jansedivy@abdoc.net>
2015-03-20 20:57:32 +0100

    implement basic version of slide-out sidebar

--- app/app.jsx
+++ app/app.jsx
@@ -2,22 +2,45 @@ var React = require('react');
 var Router = require('react-router');
 
 var App = React.createClass({
+  getInitialState() {
+    return {
+      showSidebar: false
+    };
+  },
+
+  handleClick(e) {
+    e.preventDefault();
+    this.setState({ showSidebar: !this.state.showSidebar });
+  },
+
   render() {
+    var containerClass = 'main-container';
+    if (this.state.showSidebar) {
+      containerClass += ' show-sidebar';
+    }
+
     return (
-      <div className="App">
-        <header>
-          <div className="inside">
-            <h1>Reddit app</h1>
-            <ul>
-              <li><Router.Link to="/">Favorites</Router.Link></li>
-              <li><Router.Link to="/search">Search</Router.Link></li>
-            </ul>
+      <div className={containerClass}>
+        <div className="app">
+          <header>
+            <div className="inside">
+              <h1>Reddit app</h1>
+              <ul>
+                <li><a href="#" onClick={this.handleClick}>Toggle sidebar</a></li>
+                <li><Router.Link to="/">Favorites</Router.Link></li>
+                <li><Router.Link to="/search">Search</Router.Link></li>
+              </ul>
+            </div>
+          </header>
+          <div className="content">
+            <div className="inside">
+              <Router.RouteHandler {...this.props}/>
+            </div>
           </div>
-        </header>
-        <div className="content">
-          <div className="inside">
-            <Router.RouteHandler {...this.props}/>
-          </div>
+        </div>
+
+        <div className="sidebar">
+          <h1>Sidebar</h1>
         </div>
       </div>
     );
