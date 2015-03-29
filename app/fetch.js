baef585b3f736c99f014d55da2cbd1daf07163e4
Jan Sedivy <jansedivy@abdoc.net>
2015-03-29 17:08:29 +0200

    make default Fetch calls asyn

--- app/fetch.js
+++ app/fetch.js
@@ -2,7 +2,7 @@ var Fetch = {
   get(url) {
     return new Promise(function(resolve) {
       var request = new XMLHttpRequest();
-      request.open('GET', url);
+      request.open('GET', url, true);
       request.onload = function() {
         resolve(request.responseText);
       };
